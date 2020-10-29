bd0a2935d1983f75377827d2adbe9e80045fd47c
Derek Nixon <derek.nixon@augustwenty.com>
2020-10-29 16:30:50 -0400

    minor

--- messages/Sailboat.go
+++ messages/Sailboat.go
@@ -3,6 +3,6 @@ package messages
 // Sailboat data
 type Sailboat struct {
 	Name      string   `json:"Name"`
-	Position  Vector2D  `json:"Position"`
+	Position  Vector2D `json:"Position"`
 	Timestamp float64  `json:"Timestamp"`
 }
