ba0385307044e8b58b3c83b302b18946c88290bc
Derek Nixon <derek.nixon@augustwenty.com>
2020-10-28 23:36:23 -0400

    changed field name from  to

--- messages/Sailboat.go
+++ messages/Sailboat.go
@@ -2,7 +2,7 @@ package messages
 
 // Sailboat data
 type Sailboat struct {
-	Boat      string   `json:"Boat"`
+	Name      string   `json:"Boat"`
 	Position  Vector2D `json:"Position"`
 	Timestamp float32  `json:"Timestamp"`
 }
--- messages/Speedboat.go
+++ messages/Speedboat.go
@@ -2,7 +2,7 @@ package messages
 
 // Speedboat data
 type Speedboat struct {
-	Boat      string   `json:"Boat"`
+	Name      string   `json:"Boat"`
 	Position  Vector2D `json:"Position"`
 	Velocity  Vector2D `json:"Velocity"`
 	Timestamp float32  `json:"Timestamp"`
