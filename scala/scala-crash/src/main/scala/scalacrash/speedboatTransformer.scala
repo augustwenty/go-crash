1015f2e7591c528c26a059eb3f35ce4181aeb2bc
billstests <bill.ruane@augustwenty.com>
2020-11-16 16:01:57 -0500

    send processed speedboats to kafka sink

--- scala/scala-crash/src/main/scala/scalacrash/speedboatTransformer.scala
+++ scala/scala-crash/src/main/scala/scalacrash/speedboatTransformer.scala
@@ -7,7 +7,6 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema
 import java.util.Properties
 
 object speedboatTransformer extends App {
-  println("Hello, World!")
   val env = StreamExecutionEnvironment.getExecutionEnvironment
 
   val properties = new Properties()
@@ -19,31 +18,21 @@ object speedboatTransformer extends App {
                 new SimpleStringSchema,
                 properties) 
 
-  // val kafkaProducer = new FlinkKafkaProducer[String](
-  //               "localhost:9092",
-  //               "boat_data",
-  //               new SimpleStringSchema)
+  val kafkaProducer = new FlinkKafkaProducer[String](
+                "localhost:9092",
+                "boat_data",
+                new SimpleStringSchema)
 
   val speedboatTransform = transformSpeedboat(env.addSource(kafkaConsumer))
 
+  speedboatTransform.addSink(kafkaProducer)
   speedboatTransform.print()
   env.execute()
 
   def transformSpeedboat(stream: DataStream[String]) : DataStream[String] = {
-            // Count how many times a word has been read off of the tatanka Kafka topic in 15 seconds,
-            // then send that to kafka as a Tuple to the monkey topic
             stream.map(x => Speedboat.fromJSON(x))
                   .map(x => Boat.transform(x))
                   .map(x => Boat.toJSONStringBrutish(x))
-
-            // stream.filter(_ != "poop") // Remove all the poop from the topic
-            //     .map { (_, 1) }                       // Convert each element to a Tuple. The original string is the first element, second element is number 1
-            //     .keyBy(_._1)                          // Key the stream by the first element in the Tuple
-            //     // .timeWindow(Time.seconds(15))         // Batch the steam into 15 second segments
-            //     .sum(1)                               // Sum the second element in the Tuple with the second element from the previous Tuple
-            //     .map (_.toString())                   // Convert the Tuple to a string for Kafka
         }
-  
-  // def getSpeedboatsFromStream(boatJson: String): 
 }
 
