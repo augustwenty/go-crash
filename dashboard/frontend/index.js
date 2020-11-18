a8ce8faa437f1397a78b88a81cb8ebb46c8d7e45
Eric Coleman <eric@epiccoleman.com>
2020-11-18 13:29:16 -0500

    color boats by type

--- dashboard/frontend/index.js
+++ dashboard/frontend/index.js
@@ -1,5 +1,5 @@
-const SAMPLE_BOAT = { "Name": "Slice of Life", "Position": { "x": 200, "y": 400 }, "Velocity": { "x": 0.36431035, "y": 0.5753303 }, "Timestamp": 0.3 }
-const SAMPLE_BOAT_2 = { "Name": "Stugots", "Position": { "x": 21, "y": 40 }, "Velocity": { "x": 0.36431035, "y": -0.5753303 }, "Timestamp": 0.3 }
+// const SAMPLE_BOAT = { "Name": "Slice of Life", "Position": { "x": 200, "y": 400 }, "Velocity": { "x": 0.36431035, "y": 0.5753303 }, "Timestamp": 0.3 }
+// const SAMPLE_BOAT_2 = { "Name": "Stugots", "Position": { "x": 21, "y": 40 }, "Velocity": { "x": 0.36431035, "y": -0.5753303 }, "Timestamp": 0.3 }
 
 function onMessageReceived(messageData){
     console.log('Message from server ', messageData);
@@ -34,9 +34,11 @@ function updateBoatInState(boat) {
 function render(timestamp) {
     stateP.innerText = JSON.stringify(SimulationState, null, 2)
 
+    // clear canvas
     let canvasCtx = canvas.getContext("2d");
     canvasCtx.clearRect(0,0,1000,1000)
 
+    // render boats
     for (const boat in SimulationState) {
         renderBoat(canvasCtx, SimulationState[boat])
     }
@@ -58,7 +60,15 @@ function renderBoat(canvasCtx, boat) {
     canvasCtx.rotate(boatHeading);
     canvasCtx.translate(-boatX, -boatY);
 
-    canvasCtx.fillStyle = "#9a9a9a"
+    // color boat based on type
+    let boatType = boat["Type"]
+    if (boatType == "Sailboat") {
+        canvasCtx.fillStyle = "#00FF00"
+    } else if (boatType == "speedboat") {
+        canvasCtx.fillStyle = "#FF0000"
+    } else {
+        canvasCtx.fillStyle = "#9a9a9a"
+    }
     canvasCtx.fillRect(boatX - 20 , boatY -5, 40, 10);
 
     //reset context
